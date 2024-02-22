e5d0f797789956702be855923253cef992fd61ed
Alessio Scarfone <alessioscarfone93@gmail.com>
2024-02-22 09:04:14 +0100

    feat: buy me a coffee btn

---
+++ .vitepress/theme/components/BuyMeACoffeeBtn.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+const props = defineProps({
+    title: { type: String, required: true, default: "Buy me a coffee" },
+    url: { type: String, required: true }
+})
+</script>
+
+<template>
+    <a class="bmc-button" target="_blank" :href="url">
+        <img src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy me a coffee">
+        <span class="text">{{ title }}</span>
+    </a>
+</template>
+  
+  
+<style scoped>
+.bmc-button {
+    width: fit-content;
+    min-height: 44px;
+    min-width: 44px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    line-height: 38px;
+    font-size: 14px;
+    font-weight: 600;
+    padding: 0.3rem 0.5rem;
+    border-radius: 8px;
+    margin-inline: 1rem;
+
+    font-family: var(--vp-font-family-base);
+    border-color: var(--vp-button-brand-border);
+    color: var(--vp-button-brand-text);
+    background-color: var(--vp-button-brand-bg);
+
+}
+
+.bmc-button img {
+    margin-right: 6px;
+}
+
+.bmc-button .text {
+    padding-right: 2px;
+}
+
+@media (max-width: 640px) {
+    .text {
+        display: none;
+    }
+
+    .bmc-button img {
+        margin-right: 0;
+    }
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,12 +3,15 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import BuyMeACoffeeBtn from './components/BuyMeACoffeeBtn.vue';
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      //TODO: pass correct url
+      'nav-bar-content-after': () => h(BuyMeACoffeeBtn, { title: "Buy me a coffee", url: "https://www.buymeacoffee.com" })
     })
   },
   enhanceApp({ app, router, siteData }) {
