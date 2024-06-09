e81a637bc7a6d1d3339501a4139a574cf27e6f4a
Alessio Scarfone <alessioscarfone93@gmail.com>
2024-06-09 17:19:13 +0200

    feat: add buy me a coffee button

--- .vitepress/constants.ts
+++ .vitepress/constants.ts
@@ -1,6 +1,6 @@
 export const GITHUB_LINK = "https://github.com/AlessioScarfone";
-export const BUY_ME_A_COFFEE = "https://www.buymeacoffee.com";
+export const BUY_ME_A_COFFEE = 'https://ko-fi.com/G2G5Z4O75';
 
 export const featureFlag = {
-    BUY_ME_A_COFFEE: false
+    BUY_ME_A_COFFEE: true
 }
--- .vitepress/theme/components/BuyMeACoffeeBtn.vue
+++ .vitepress/theme/components/BuyMeACoffeeBtn.vue
@@ -7,24 +7,25 @@ const props = defineProps({
 
 <template>
     <a class="bmc-button" target="_blank" :href="url">
-        <img src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy me a coffee">
+        <img src="https://storage.ko-fi.com/cdn/brandasset/kofi_s_logo_nolabel.png" alt="Buy me a coffee">
         <span class="text">{{ title }}</span>
     </a>
 </template>
-  
-  
+
 <style scoped>
 .bmc-button {
     width: fit-content;
-    min-height: 44px;
+    height: 44px;
     min-width: 44px;
+    max-height: 44px;
+
     display: flex;
     justify-content: space-evenly;
     align-items: center;
     line-height: 38px;
     font-size: 14px;
     font-weight: 600;
-    padding: 0.3rem 0.5rem;
+    padding: 0.3rem 0.5rem 0.3rem 0.4rem;
     border-radius: 8px;
     margin-inline: 1rem;
     text-decoration: none;
@@ -41,19 +42,25 @@ const props = defineProps({
 
 .bmc-button img {
     margin-right: 6px;
+    object-fit: contain;
+    max-height: 100%;
 }
 
 .bmc-button .text {
     padding-right: 2px;
 }
 
-@media (max-width: 640px) {
+@media screen and (max-width: 640px) {
     .text {
         display: none;
     }
 
-    .bmc-button img {
-        margin-right: 0;
+    .bmc-button {
+        padding: 0.1rem;
+        margin-inline: 0;
+        img {
+            margin-right: 0;
+        }
     }
 }
 </style>
